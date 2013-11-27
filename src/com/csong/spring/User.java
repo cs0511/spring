d9f367c23f61c67d4d8adb8e755b7b87afa602d0
cs0511 <cs0511@126.com>
2013-11-27 23:17:59 +0800

    init spring project

---
+++ src/com/csong/spring/User.java
@@ -0,0 +1,33 @@
+package com.csong.spring;
+
+public class User {
+	private String name;
+	private String pwd;
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return pwd
+	 */
+	public String getPwd() {
+		return pwd;
+	}
+	/**
+	 * @param pwd the pwd to set
+	 */
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+
+
+}
